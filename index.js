d36260e3a751905bec485c3f13354ad1047c7d43
radubrehar <radu@jslog.com>
2015-03-11 11:01:43 +0200

    move dom dependency at runtime

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var curry   = require('./curry')
-var matches = require('./matches')
+var matches
 
 module.exports = curry(function(selector, node){
+
+	matches = matches || require('./matches')
+
     while (node = node.parentElement){
         if (matches.call(node, selector)){
             return node
